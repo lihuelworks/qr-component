f9511dd4b63126e7fe9ebfc1abc1d9f2a0d9fd2f
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-02-19 23:07:35 -0300

    added files

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/_variables.scss";@import "@/styles/styles.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    open: true,
+  },
+});
