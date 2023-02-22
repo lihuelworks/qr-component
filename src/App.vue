efefcb7a2886f8d2eafd757475882e821b686746
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-02-21 23:51:10 -0300

    barebones scss and app structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <h1>
-   hello there!
-  </h1>
+  <div class="qr_card">
+    <img
+      class="qr_image"
+      src=".\assets\image-qr-code.png"
+      alt="qr code to frontend masters"
+    />
+    <h3>Improve your front-end skills by building projects</h3>
+    <p>
+      Scan the QR code to visit Frontend Master and take your coding skills to
+      the next level
+    </p>
+  </div>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
