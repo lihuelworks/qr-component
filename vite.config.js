c63d1d7fb971825258da26b0117c36f3c9245f15
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-02-22 21:43:48 -0300

    fixed base pointing to repo name instead of dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/qr-component/",
+  base: "/dist/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
