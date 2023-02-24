5494bc6c17d3a9cbe23df8218d9fff9c76f2f57a
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-02-24 11:30:43 -0300

    Fixed App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="qr_card">
     <img
-      class="qr_image"
+      class="qr_card-image"
       src=".\assets\image-qr-code.png"
       alt="qr code to frontend masters"
     />
-    <h3>Improve your front-end skills by building projects</h3>
-    <p>
-      Scan the QR code to visit Frontend Master and take your coding skills to
-      the next level
-    </p>
+    <div class="qr_card-text">
+      <h3>Improve your front-end skills by building projects</h3>
+      <p>
+        Scan the QR code to visit Frontend Master and take your coding skills to
+        the next level
+      </p>
+    </div>
   </div>
 </template>
 
